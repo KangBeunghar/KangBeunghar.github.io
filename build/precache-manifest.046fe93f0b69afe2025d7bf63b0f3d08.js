5a0a638ee3df1fc42710296772ce016be5cbbdc4
KangBeunghar <adityanurjohansyah@gmail.com>
2020-06-10 09:49:55 +0700

    initial
    initial

---
+++ build/precache-manifest.046fe93f0b69afe2025d7bf63b0f3d08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a87e521a64e639313f6a1c9e8956564",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1cb5eaee634e97a98991",
+    "url": "/static/css/main.9c405617.chunk.css"
+  },
+  {
+    "revision": "8a93142ed3dc86391619",
+    "url": "/static/js/2.78e38360.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.78e38360.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cb5eaee634e97a98991",
+    "url": "/static/js/main.5a84011f.chunk.js"
+  },
+  {
+    "revision": "d7bd3f4a380116bfaedc",
+    "url": "/static/js/runtime-main.de7f2bea.js"
+  }
+]);
